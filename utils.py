f80937fb48315b3bbc69445b4da48f5e94aaf934
James Holloway <james.holloway94@gmail.com>
2026-04-29 07:50:08 +0000

    advance cdk app deploy time into config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-04-29 06:16:38 UTC
+# Last sync: 2026-04-29 07:50:08 UTC
