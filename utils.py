6fbe68aaf97b4c0e9eb5d98df737fed7c30b496d
James Holloway <james.holloway94@gmail.com>
2026-05-01 03:22:58 +0000

    publish lambda alias promotion marker after release

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-01 02:43:13 UTC
+# Last sync: 2026-05-01 03:22:58 UTC
