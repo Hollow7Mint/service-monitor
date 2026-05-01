d8bc994f3eb4d42a4d72f85a93be2c905d6c3d13
James Holloway <james.holloway94@gmail.com>
2026-05-01 16:44:17 +0000

    queue redis keyspace sync after config update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-01 15:58:51 UTC
+# Last sync: 2026-05-01 16:44:17 UTC
