f8154ed38cbe5be825ec8fc59214ef199b603c02
James Holloway <james.holloway94@gmail.com>
2026-05-01 11:39:40 +0000

    anchor cloudwatch alarm threshold time in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-01 10:49:53 UTC
+# Last sync: 2026-05-01 11:39:40 UTC
