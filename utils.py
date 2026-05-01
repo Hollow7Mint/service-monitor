8ef4771483e8b35978d5ad68a2bd1d753a3a10bd
James Holloway <james.holloway94@gmail.com>
2026-05-01 21:17:52 +0000

    snapshot bastion session time for security audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-01 20:43:52 UTC
+# Last sync: 2026-05-01 21:17:52 UTC
