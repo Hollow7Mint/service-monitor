5ab492fcbb75d65fcc381e449baa56d1b732ab83
James Holloway <james.holloway94@gmail.com>
2026-05-03 21:05:51 +0000

    retain cloudwatch metric filter time for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-03 19:36:37 UTC
+# Last sync: 2026-05-03 21:05:51 UTC
