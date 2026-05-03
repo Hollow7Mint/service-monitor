ccdaf0d74a317a3f3b1aaf776f3124814f10ee93
James Holloway <james.holloway94@gmail.com>
2026-05-03 12:05:55 +0000

    restore ecr scan result time for image audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-03 10:22:29 UTC
+# Last sync: 2026-05-03 12:05:55 UTC
