d42c993ea720dca9cb2e490402ebb6b778cf9c53
James Holloway <james.holloway94@gmail.com>
2026-05-07 11:20:20 +0000

    close post-deploy heartbeat for infra pipeline

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-07 10:52:13 UTC
+# Last sync: 2026-05-07 11:20:20 UTC
