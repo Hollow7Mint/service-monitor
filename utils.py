88b3ec0af2131dd52066d3ba7e3980b18adf955a
James Holloway <james.holloway94@gmail.com>
2026-05-09 22:30:57 +0000

    post ssh key rotation heartbeat to config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-09 21:49:09 UTC
+# Last sync: 2026-05-09 22:30:57 UTC
