ec0925422610a4516d5a8f905c6e00ca08657d69
James Holloway <james.holloway94@gmail.com>
2026-05-10 15:40:12 +0000

    annotate ec2 ami bake time for deploy record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-10 14:30:42 UTC
+# Last sync: 2026-05-10 15:40:12 UTC
