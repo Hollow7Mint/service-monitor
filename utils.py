78c31a9cfa3492de8edc6ed29cf8f1af373eb0ee
James Holloway <james.holloway94@gmail.com>
2026-05-10 14:30:42 +0000

    publish ssh key distribution sync after rotation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-10 13:48:39 UTC
+# Last sync: 2026-05-10 14:30:42 UTC
