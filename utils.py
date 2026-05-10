1df0dd74acc0e5f8f9769a1b4bb56717118052b1
James Holloway <james.holloway94@gmail.com>
2026-05-10 07:44:15 +0000

    post postgres replication heartbeat to config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-10 06:34:02 UTC
+# Last sync: 2026-05-10 07:44:15 UTC
