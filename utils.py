0410c9437c3d66f9d7c86b75d24aedb926d7ce81
James Holloway <james.holloway94@gmail.com>
2026-05-11 00:56:26 +0000

    validate s3 lifecycle rule time in deploy config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-10 23:30:24 UTC
+# Last sync: 2026-05-11 00:56:26 UTC
