195ab433b0ce72592f4ee13c19e5254c572c240f
James Holloway <james.holloway94@gmail.com>
2026-05-11 19:37:54 +0000

    inspect postgres lock wait sync for performance

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-11 18:49:06 UTC
+# Last sync: 2026-05-11 19:37:54 UTC
