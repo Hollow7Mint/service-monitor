0592158573d974843de76dd753d9baf5ec35a6be
James Holloway <james.holloway94@gmail.com>
2026-05-13 18:56:53 +0000

    emit lambda function deploy heartbeat for release

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-13 17:17:20 UTC
+# Last sync: 2026-05-13 18:56:53 UTC
