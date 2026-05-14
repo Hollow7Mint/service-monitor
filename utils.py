95fdea265eff98b32ce4894c598872ba26faa469
James Holloway <james.holloway94@gmail.com>
2026-05-14 14:26:36 +0000

    flush ec2 fleet inventory marker after scan

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-14 14:00:49 UTC
+# Last sync: 2026-05-14 14:26:36 UTC
