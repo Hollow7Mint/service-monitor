08e153d537af2ec947afc40d96a9187a218bed56
James Holloway <james.holloway94@gmail.com>
2026-05-14 11:24:43 +0000

    trace mysql table statistics sync for fleet audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-14 10:51:45 UTC
+# Last sync: 2026-05-14 11:24:43 UTC
