9869c3d6e6180c131a7cc47d5d48b2a0e3eac214
James Holloway <james.holloway94@gmail.com>
2026-05-14 16:49:55 +0000

    capture rds snapshot completion time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-14 15:49:29 UTC
+# Last sync: 2026-05-14 16:49:55 UTC
