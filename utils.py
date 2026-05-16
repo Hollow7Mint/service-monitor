445680c5f4635bc3a94b3a2c192db7ead8512a6e
James Holloway <james.holloway94@gmail.com>
2026-05-16 02:28:51 +0000

    persist connection string refresh in config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-16 01:49:17 UTC
+# Last sync: 2026-05-16 02:28:51 UTC
