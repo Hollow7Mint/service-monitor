f56cff71fdcc1717fe510571b349df75d143484b
James Holloway <james.holloway94@gmail.com>
2026-05-16 08:03:22 +0000

    recheck mongodb write concern sync after update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-16 06:14:37 UTC
+# Last sync: 2026-05-16 08:03:22 UTC
