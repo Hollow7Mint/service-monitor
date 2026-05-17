556691aae39d2222241487008cb642fcae5fc6e0
James Holloway <james.holloway94@gmail.com>
2026-05-17 08:19:30 +0000

    bridge lambda env var sync through config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-17 07:38:35 UTC
+# Last sync: 2026-05-17 08:19:30 UTC
