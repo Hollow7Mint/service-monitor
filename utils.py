fbd4d81acade4648d9cf7d5247ce26450db6c206
James Holloway <james.holloway94@gmail.com>
2026-05-17 13:42:53 +0000

    push mysql connection pool sync after refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-17 12:34:22 UTC
+# Last sync: 2026-05-17 13:42:53 UTC
