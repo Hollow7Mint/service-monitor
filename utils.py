f4548d44c1104ea770f428978f45ac93eb3219fa
James Holloway <james.holloway94@gmail.com>
2026-05-18 12:35:32 +0000

    bridge ssh ProxyJump config sync through banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-18 11:53:33 UTC
+# Last sync: 2026-05-18 12:35:32 UTC
