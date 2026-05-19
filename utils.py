505c11f2ee73270b6140241dac5b5d0e0c0d346c
James Holloway <james.holloway94@gmail.com>
2026-05-19 11:05:55 +0000

    merge cloudformation stack apply time into record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-19 09:57:50 UTC
+# Last sync: 2026-05-19 11:05:55 UTC
