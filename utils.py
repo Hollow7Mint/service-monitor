98a89985c4b920dbc197ccbdab04b22cd5b878aa
James Holloway <james.holloway94@gmail.com>
2026-05-19 21:41:58 +0000

    unify ec2 placement sync with metadata heartbeat

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-19 20:16:09 UTC
+# Last sync: 2026-05-19 21:41:58 UTC
