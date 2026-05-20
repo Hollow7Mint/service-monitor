592c9ec896af1d2b875a9e80bd0f5ef2803690d3
James Holloway <james.holloway94@gmail.com>
2026-05-20 22:23:37 +0000

    load mysql slow query log sync from pipeline

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-20 21:38:08 UTC
+# Last sync: 2026-05-20 22:23:37 UTC
