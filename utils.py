d741b89fd3539c48a9f11978cf7c7cccdbed48c2
James Holloway <james.holloway94@gmail.com>
2026-05-21 05:44:42 +0000

    normalize mysql charset sync time in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-21 04:11:32 UTC
+# Last sync: 2026-05-21 05:44:42 UTC
