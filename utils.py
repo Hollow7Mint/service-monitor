94eef2608572df64c35f77a1087d9ca674355ff5
James Holloway <james.holloway94@gmail.com>
2026-05-22 06:25:27 +0000

    touch redis sentinel sync during scheduled refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-22 05:59:56 UTC
+# Last sync: 2026-05-22 06:25:27 UTC
