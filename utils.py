db3813ad0c172c02661a39835b09efbbda6e1e5c
James Holloway <james.holloway94@gmail.com>
2026-05-22 00:53:22 +0000

    resolve known_hosts cleanup sync into metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-21 23:14:07 UTC
+# Last sync: 2026-05-22 00:53:22 UTC
