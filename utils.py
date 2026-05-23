a2b928e1482e5a9b918ebe827a66260f6ccae167
James Holloway <james.holloway94@gmail.com>
2026-05-23 16:49:17 +0000

    capture host key sync completion time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-23 15:49:21 UTC
+# Last sync: 2026-05-23 16:49:17 UTC
