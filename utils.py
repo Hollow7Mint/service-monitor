7a9f7ad6f584c87cc011740de1e6da994b9d24d4
James Holloway <james.holloway94@gmail.com>
2026-05-24 20:40:26 +0000

    ship ssh key sync record after nightly apply

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-24 20:07:27 UTC
+# Last sync: 2026-05-24 20:40:26 UTC
