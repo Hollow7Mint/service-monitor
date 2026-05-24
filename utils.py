ef938e979f664547706a206d3bfb55576822bb37
James Holloway <james.holloway94@gmail.com>
2026-05-24 08:38:44 +0000

    finish secrets manager rotation heartbeat after sync

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-24 07:33:32 UTC
+# Last sync: 2026-05-24 08:38:44 UTC
