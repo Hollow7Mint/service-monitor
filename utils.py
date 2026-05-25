a953d22d9bbdd1b9974dfb0cf2fd1d9b0503307f
James Holloway <james.holloway94@gmail.com>
2026-05-25 23:58:19 +0000

    submit ssh certificate marker after rotation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-25 22:40:01 UTC
+# Last sync: 2026-05-25 23:58:19 UTC
