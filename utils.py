9271d5ec693549c98527597dd54a1a3a847d9c7d
James Holloway <james.holloway94@gmail.com>
2026-05-26 05:22:28 +0000

    snapshot iam role binding time for audit trail

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-26 04:13:01 UTC
+# Last sync: 2026-05-26 05:22:28 UTC
