504a4350180378c4f3d454210a61e417ba5d1a14
James Holloway <james.holloway94@gmail.com>
2026-05-27 15:29:25 +0000

    trace ssh identity file sync for key audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-27 14:58:36 UTC
+# Last sync: 2026-05-27 15:29:25 UTC
