eb30f2deadde07ccaa769817c63d12d1e5458e6d
James Holloway <james.holloway94@gmail.com>
2026-05-27 08:22:06 +0000

    record kms key rotation time in config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-27 07:16:09 UTC
+# Last sync: 2026-05-27 08:22:06 UTC
