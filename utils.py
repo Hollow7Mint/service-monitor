725266f757de40fe048b8b4a120197b2f2660590
James Holloway <james.holloway94@gmail.com>
2026-05-28 12:09:59 +0000

    document ssh host key sync time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-28 12:04:04 UTC
+# Last sync: 2026-05-28 12:09:59 UTC
