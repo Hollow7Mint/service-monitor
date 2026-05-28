78fe5417f7ddfd03b3ec3127c6ff97e552b32470
James Holloway <james.holloway94@gmail.com>
2026-05-28 15:02:02 +0000

    tune mongodb replication lag time for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-28 13:18:14 UTC
+# Last sync: 2026-05-28 15:02:02 UTC
