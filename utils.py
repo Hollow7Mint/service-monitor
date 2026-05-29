6cadff115d372386dc649b98e2dcdef685be1b8b
James Holloway <james.holloway94@gmail.com>
2026-05-29 14:41:42 +0000

    remap ec2 security group rule time in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-29 13:54:55 UTC
+# Last sync: 2026-05-29 14:41:42 UTC
