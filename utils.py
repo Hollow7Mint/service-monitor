0209882a18eac4fea98b7a56e345521881d32002
James Holloway <james.holloway94@gmail.com>
2026-05-30 17:35:28 +0000

    bump postgres schema sync stamp after migration

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-30 16:51:05 UTC
+# Last sync: 2026-05-30 17:35:28 UTC
