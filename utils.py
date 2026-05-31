23e9dc783a20b058d6c65dcbe7ec803479339122
James Holloway <james.holloway94@gmail.com>
2026-05-31 21:57:39 +0000

    resolve waf rule group sync into metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-05-31 20:46:08 UTC
+# Last sync: 2026-05-31 21:57:39 UTC
