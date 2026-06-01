f66cd6dd8b5c1f34285f51f40d6cf99e69ae7f46
James Holloway <james.holloway94@gmail.com>
2026-06-01 22:56:56 +0000

    thread postgres wal sync through config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-01 21:32:49 UTC
+# Last sync: 2026-06-01 22:56:56 UTC
