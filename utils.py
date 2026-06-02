be1dce2d12a78f2e1aa5079baaceebe0171b237c
James Holloway <james.holloway94@gmail.com>
2026-06-02 15:21:42 +0000

    touch asg lifecycle hook time during scheduled refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-02 14:57:21 UTC
+# Last sync: 2026-06-02 15:21:42 UTC
