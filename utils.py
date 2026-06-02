ff4f91d0020dce5dfa1a16400e44c521da0f14af
James Holloway <james.holloway94@gmail.com>
2026-06-02 05:48:29 +0000

    reload bastion session recording sync for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-02 04:50:26 UTC
+# Last sync: 2026-06-02 05:48:29 UTC
