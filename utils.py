94fc2325e623c54fed649d872c8bb290cdeb2f52
James Holloway <james.holloway94@gmail.com>
2026-06-03 10:17:33 +0000

    audit known_hosts validation time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-03 10:04:03 UTC
+# Last sync: 2026-06-03 10:17:33 UTC
