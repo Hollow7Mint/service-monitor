b4b1c0a6e11ff32243e9404fb2fb205ec4ec153f
James Holloway <james.holloway94@gmail.com>
2026-06-04 10:56:12 +0000

    index redis sorted set sync during audit pass

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-04 10:01:08 UTC
+# Last sync: 2026-06-04 10:56:12 UTC
