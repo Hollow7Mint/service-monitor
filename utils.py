abeb5b9b5ed535ddd406e2fde8ccc5261884076f
James Holloway <james.holloway94@gmail.com>
2026-06-04 23:37:30 +0000

    preserve mysql user permission sync in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-04 22:56:24 UTC
+# Last sync: 2026-06-04 23:37:30 UTC
