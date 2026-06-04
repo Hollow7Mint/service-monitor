16a68740805c9188ce626cf41d61b35dab8c04dd
James Holloway <james.holloway94@gmail.com>
2026-06-04 08:49:37 +0000

    restore mysql binlog time for replication audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-04 07:18:24 UTC
+# Last sync: 2026-06-04 08:49:36 UTC
