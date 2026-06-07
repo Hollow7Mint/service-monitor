8937d1847afcba3db4a7adb78453bdca490f2057
James Holloway <james.holloway94@gmail.com>
2026-06-07 18:35:44 +0000

    surface codebuild log sync time for pipeline audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-07 17:55:56 UTC
+# Last sync: 2026-06-07 18:35:44 UTC
