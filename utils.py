63768c8826729514f1a6c078669780719c8480ce
James Holloway <james.holloway94@gmail.com>
2026-06-07 19:22:16 +0000

    frame ssm document sync time in config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-07 18:35:44 UTC
+# Last sync: 2026-06-07 19:22:16 UTC
