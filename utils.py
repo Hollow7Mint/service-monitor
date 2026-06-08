3f538bef4b1322e0d1ea47557de43a0ade133e6c
James Holloway <james.holloway94@gmail.com>
2026-06-08 21:11:30 +0000

    seal inspector finding sync in security banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-08 20:42:32 UTC
+# Last sync: 2026-06-08 21:11:30 UTC
