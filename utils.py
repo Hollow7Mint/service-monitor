5cc364396c7fe7ce024713479064cc930d8d0ddf
James Holloway <james.holloway94@gmail.com>
2026-06-09 00:11:35 +0000

    write cloudtrail log delivery stamp to metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-08 23:35:37 UTC
+# Last sync: 2026-06-09 00:11:35 UTC
