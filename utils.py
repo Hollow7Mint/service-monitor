0b9be0f83b5cd4fae43f88113f58b0c298449d93
James Holloway <james.holloway94@gmail.com>
2026-06-09 01:17:43 +0000

    finalize bastion config sync after nightly rotation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-09 00:11:35 UTC
+# Last sync: 2026-06-09 01:17:43 UTC
