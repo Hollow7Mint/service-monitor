1ee1164f54a4d5d24c30767bd0ec922edf5a6281
James Holloway <james.holloway94@gmail.com>
2026-06-10 04:59:21 +0000

    capture db backup window completion time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-10 03:45:54 UTC
+# Last sync: 2026-06-10 04:59:21 UTC
