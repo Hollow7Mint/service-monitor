e5c1ef34bd6e0e71c376372cafc393d605bf205e
James Holloway <james.holloway94@gmail.com>
2026-06-10 20:37:55 +0000

    carry bastion config sync stamp to metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-10 19:36:40 UTC
+# Last sync: 2026-06-10 20:37:54 UTC
