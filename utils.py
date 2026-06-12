24b334b6650b17fd50d9670581c84ff7e986125b
James Holloway <james.holloway94@gmail.com>
2026-06-12 15:25:55 +0000

    adjust redis ttl sync stamp after cache flush

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-12 14:57:49 UTC
+# Last sync: 2026-06-12 15:25:55 UTC
