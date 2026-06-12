dcdb72d75d1eda90d5ecc8f00d591b237fe975e4
James Holloway <james.holloway94@gmail.com>
2026-06-12 17:26:12 +0000

    inject redis eviction sync marker into config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-12 17:06:18 UTC
+# Last sync: 2026-06-12 17:26:12 UTC
