ddf763e18fa0b09b5099ed1438552ab548054fe8
James Holloway <james.holloway94@gmail.com>
2026-06-12 22:48:40 +0000

    normalize ecs cluster config time in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-12 21:59:46 UTC
+# Last sync: 2026-06-12 22:48:40 UTC
