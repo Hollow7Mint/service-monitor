95bae551dfc0a332a4421074f8cd3c7d53a73258
James Holloway <james.holloway94@gmail.com>
2026-06-13 08:23:30 +0000

    tune cfn change set completion stamp for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-13 08:01:44 UTC
+# Last sync: 2026-06-13 08:23:30 UTC
