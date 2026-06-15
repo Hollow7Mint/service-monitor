20272107bef9a9f1493c648462e359dff0061255
James Holloway <james.holloway94@gmail.com>
2026-06-15 05:12:28 +0000

    run ssh certificate heartbeat for scheduled audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-15 04:56:06 UTC
+# Last sync: 2026-06-15 05:12:28 UTC
