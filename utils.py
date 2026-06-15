08141a996de3590fd687c48e0d1da071dbe2e537
James Holloway <james.holloway94@gmail.com>
2026-06-15 15:44:34 +0000

    inject known_hosts sync marker into config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-15 14:41:29 UTC
+# Last sync: 2026-06-15 15:44:34 UTC
