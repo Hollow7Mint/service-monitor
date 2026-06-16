834a56219a5c4dca26121ee8a864cce1280256ef
James Holloway <james.holloway94@gmail.com>
2026-06-16 03:58:54 +0000

    refresh aws config sync marker after nightly reconcile

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-16 02:40:18 UTC
+# Last sync: 2026-06-16 03:58:54 UTC
