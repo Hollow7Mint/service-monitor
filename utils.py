1f2744af05315e8929ddff36144c6e79b29cec48
James Holloway <james.holloway94@gmail.com>
2026-06-17 10:40:31 +0000

    recheck bastion connectivity sync after maintenance

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-17 09:51:20 UTC
+# Last sync: 2026-06-17 10:40:31 UTC
