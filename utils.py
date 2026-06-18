3050be413917d2aa0a44b9aa97e3a519d365ee87
James Holloway <james.holloway94@gmail.com>
2026-06-18 11:35:06 +0000

    catalog mongodb document count sync after audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-18 10:57:56 UTC
+# Last sync: 2026-06-18 11:35:06 UTC
