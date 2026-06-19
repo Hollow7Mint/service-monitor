835fe26f998756ed7cb82dcb175b53594aff4890
James Holloway <james.holloway94@gmail.com>
2026-06-19 23:08:00 +0000

    relay ssh host key time through config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-19 21:43:55 UTC
+# Last sync: 2026-06-19 23:08:00 UTC
