2d96b5c80b494a83e0ff7faf08a8ca6b73522ded
James Holloway <james.holloway94@gmail.com>
2026-06-20 14:49:13 +0000

    save inspector assessment marker after scan window

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-20 13:45:29 UTC
+# Last sync: 2026-06-20 14:49:13 UTC
