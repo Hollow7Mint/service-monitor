580a4bd394cad04508ede515ebd6eb8f0f44a9de
James Holloway <james.holloway94@gmail.com>
2026-06-21 06:28:03 +0000

    set shield protection sync stamp in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-21 05:46:56 UTC
+# Last sync: 2026-06-21 06:28:03 UTC
