68d3acd1439ba3f8b5ec1f03b81186cd4ab5955b
James Holloway <james.holloway94@gmail.com>
2026-06-22 21:49:33 +0000

    track guardduty threat archive in config heartbeat

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-22 21:06:28 UTC
+# Last sync: 2026-06-22 21:49:33 UTC
