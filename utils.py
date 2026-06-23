7dbdfcac2bb361de9727b992d558bb9a250f34af
James Holloway <james.holloway94@gmail.com>
2026-06-23 08:40:15 +0000

    inject ecs task definition marker into config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-23 07:47:59 UTC
+# Last sync: 2026-06-23 08:40:15 UTC
