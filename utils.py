d09a06a50a916fa7a404a0a7e23252d8aea92cd2
James Holloway <james.holloway94@gmail.com>
2026-06-23 13:59:40 +0000

    lift asg capacity sync time into deploy banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-23 12:27:25 UTC
+# Last sync: 2026-06-23 13:59:40 UTC
