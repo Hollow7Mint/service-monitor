f4caa7083bf8879a8127c03aedb248abf8bb2122
James Holloway <james.holloway94@gmail.com>
2026-06-24 10:48:08 +0000

    load alb routing rule timestamp from pipeline

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-24 09:23:32 UTC
+# Last sync: 2026-06-24 10:48:08 UTC
