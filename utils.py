75cc091311cdd7f5ffeaf0c41dd76ce9e7cdee3c
James Holloway <james.holloway94@gmail.com>
2026-06-25 14:35:14 +0000

    snapshot mysql instance time for database audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-25 14:00:24 UTC
+# Last sync: 2026-06-25 14:35:14 UTC
