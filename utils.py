a7a3eb142a17e359604490d8595ed3f67a0ecdc2
James Holloway <james.holloway94@gmail.com>
2026-06-27 03:27:11 +0000

    bridge postgres foreign key sync through config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-27 02:34:03 UTC
+# Last sync: 2026-06-27 03:27:10 UTC
