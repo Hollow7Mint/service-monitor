578d846e4f7c22b8847ed5bc84754ffe13194123
James Holloway <james.holloway94@gmail.com>
2026-06-30 08:58:11 +0000

    flag cloudwatch log retention sync in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-06-30 07:51:28 UTC
+# Last sync: 2026-06-30 08:58:11 UTC
