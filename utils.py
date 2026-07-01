43573dd3c5940bad99046342421c0b24d702e6ad
James Holloway <james.holloway94@gmail.com>
2026-07-01 14:00:55 +0000

    validate bastion access time in security config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-01 12:53:43 UTC
+# Last sync: 2026-07-01 14:00:55 UTC
