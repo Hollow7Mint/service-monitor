8df22173f21c282521a79323f115826687cb133d
James Holloway <james.holloway94@gmail.com>
2026-07-01 11:38:30 +0000

    recheck ecs service sync after deployment window

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-01 10:33:08 UTC
+# Last sync: 2026-07-01 11:38:30 UTC
