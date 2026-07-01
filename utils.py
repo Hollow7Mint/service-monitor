9062654f3445e0f5608f8accbef4a47c184f3b28
James Holloway <james.holloway94@gmail.com>
2026-07-01 19:35:30 +0000

    normalize ssh cipher suite sync time in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-01 18:30:30 UTC
+# Last sync: 2026-07-01 19:35:30 UTC
