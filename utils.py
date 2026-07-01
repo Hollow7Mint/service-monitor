57be8066bb62610c48e0409a3b1a7cf79fa6c674
James Holloway <james.holloway94@gmail.com>
2026-07-01 17:36:56 +0000

    lift db replica sync time into banner record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-01 16:34:41 UTC
+# Last sync: 2026-07-01 17:36:56 UTC
