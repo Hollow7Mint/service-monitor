a3aaa380e0c2daa28b807b08f81957d56a42a40f
James Holloway <james.holloway94@gmail.com>
2026-07-02 14:34:18 +0000

    trace asg launch template time for fleet audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-02 13:47:07 UTC
+# Last sync: 2026-07-02 14:34:18 UTC
