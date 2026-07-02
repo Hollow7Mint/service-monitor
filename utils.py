5f6a295630983487788d0f9bf262e0e0dc092cf4
James Holloway <james.holloway94@gmail.com>
2026-07-02 23:11:43 +0000

    backfill s3 versioning sync marker in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-02 22:19:50 UTC
+# Last sync: 2026-07-02 23:11:43 UTC
