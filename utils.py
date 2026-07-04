6ab67ac43851da795d570cb3921257de23a11914
James Holloway <james.holloway94@gmail.com>
2026-07-04 18:47:58 +0000

    preserve nat gateway sync time in infra banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-04 17:29:20 UTC
+# Last sync: 2026-07-04 18:47:58 UTC
