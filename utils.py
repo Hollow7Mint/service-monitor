04af02739c9d76d872351cea4556b6efd6b74f8c
James Holloway <james.holloway94@gmail.com>
2026-07-05 19:40:22 +0000

    transfer ssh config time to bastion banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-05 18:20:53 UTC
+# Last sync: 2026-07-05 19:40:22 UTC
