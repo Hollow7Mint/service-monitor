0c7a5a11f5c27a236c08c2cfb5c70e5f57384947
James Holloway <james.holloway94@gmail.com>
2026-07-06 13:39:04 +0000

    flush mongodb replica lag marker for nightly check

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-06 13:08:00 UTC
+# Last sync: 2026-07-06 13:39:03 UTC
