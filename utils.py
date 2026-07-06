789cbbd62591de674410ac6e1602dcba5a93fcac
James Holloway <james.holloway94@gmail.com>
2026-07-06 00:36:12 +0000

    chart autoscale sync time in deployment metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-05 23:53:03 UTC
+# Last sync: 2026-07-06 00:36:12 UTC
