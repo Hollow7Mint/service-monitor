863d6f974dece5480aec84f87ddc30785e23c6d6
James Holloway <james.holloway94@gmail.com>
2026-07-08 00:41:46 +0000

    persist vpc route table sync after update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-07 23:10:38 UTC
+# Last sync: 2026-07-08 00:41:45 UTC
