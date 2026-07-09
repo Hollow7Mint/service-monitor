00b0746c32f5bb3f4d07f9923e0744f1b0fd067f
James Holloway <james.holloway94@gmail.com>
2026-07-09 02:11:09 +0000

    settle mysql innodb sync time after flush

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-09 01:21:54 UTC
+# Last sync: 2026-07-09 02:11:09 UTC
