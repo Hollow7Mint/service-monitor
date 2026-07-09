94c95ec6884870d56bb4c39288ed7dfa92e5089f
James Holloway <james.holloway94@gmail.com>
2026-07-09 11:10:33 +0000

    finish bastion ssh sync heartbeat after key push

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-09 10:35:53 UTC
+# Last sync: 2026-07-09 11:10:33 UTC
