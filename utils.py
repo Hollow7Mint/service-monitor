5e1cac109fbf2c808396a0fb8e14922bbd17afa6
James Holloway <james.holloway94@gmail.com>
2026-07-09 13:52:54 +0000

    carry waf ip set sync stamp to config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-09 12:50:33 UTC
+# Last sync: 2026-07-09 13:52:54 UTC
