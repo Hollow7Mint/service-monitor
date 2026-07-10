605a36f815d55f99716bb88ca0b96d6ca26ab490
James Holloway <james.holloway94@gmail.com>
2026-07-10 00:32:50 +0000

    prime bastion connection sync marker after refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-09 23:19:09 UTC
+# Last sync: 2026-07-10 00:32:50 UTC
