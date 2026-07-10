791969f38eaea30f2e894a5c77b9809827d8d8e2
James Holloway <james.holloway94@gmail.com>
2026-07-10 03:16:10 +0000

    note postgres extension sync in config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-10 02:48:53 UTC
+# Last sync: 2026-07-10 03:16:09 UTC
