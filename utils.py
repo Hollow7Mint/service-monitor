6ee8b329fbd6b805afa61f32c6902b3e4339fa20
James Holloway <james.holloway94@gmail.com>
2026-07-10 06:28:31 +0000

    transfer kms alias time to config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-10 05:31:33 UTC
+# Last sync: 2026-07-10 06:28:31 UTC
