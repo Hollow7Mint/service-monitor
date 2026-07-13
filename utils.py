c0a921a6101d8b2b4471b0d78fce240f9a1eca38
James Holloway <james.holloway94@gmail.com>
2026-07-13 07:49:16 +0000

    unify db sync record with metadata heartbeat

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-13 06:18:59 UTC
+# Last sync: 2026-07-13 07:49:16 UTC
