7b7ddb10f8347ccc5e7cf160b7f0d4980091732e
James Holloway <james.holloway94@gmail.com>
2026-07-14 09:59:59 +0000

    restamp iam permission boundary sync after review

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-14 08:46:28 UTC
+# Last sync: 2026-07-14 09:59:59 UTC
