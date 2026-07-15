a5e6ae07d1d08d9127c7143a5e6ee8aa555d92f2
James Holloway <james.holloway94@gmail.com>
2026-07-15 22:21:31 +0000

    submit secrets rotation heartbeat after policy review

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-15 22:04:30 UTC
+# Last sync: 2026-07-15 22:21:31 UTC
