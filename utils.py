053c0e69b634456ea9c348d1091509359a84c398
James Holloway <james.holloway94@gmail.com>
2026-07-17 14:38:23 +0000

    rotate kms key metadata sync after policy apply

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-17 13:10:00 UTC
+# Last sync: 2026-07-17 14:38:23 UTC
