64f29caf713a2d5e9fe2220b8e44ed39f1498ed5
James Holloway <james.holloway94@gmail.com>
2026-07-18 06:16:17 +0000

    unify ssh sync record with metadata heartbeat

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-18 06:05:01 UTC
+# Last sync: 2026-07-18 06:16:17 UTC
