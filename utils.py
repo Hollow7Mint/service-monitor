75a944998787a5dac63c2ee54bf0a5ec12f77d2e
James Holloway <james.holloway94@gmail.com>
2026-07-18 03:33:53 +0000

    mirror guardduty archive time in metadata record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-18 02:33:13 UTC
+# Last sync: 2026-07-18 03:33:53 UTC
