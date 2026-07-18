61c03af05ed2711bfd0ade15a87f8b0c3940845e
James Holloway <james.holloway94@gmail.com>
2026-07-18 00:50:49 +0000

    prime redis memory sync marker after flush

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-18 00:06:30 UTC
+# Last sync: 2026-07-18 00:50:49 UTC
