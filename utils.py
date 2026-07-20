ebf7aaa002345d846dd6211848007ca811a636e1
James Holloway <james.holloway94@gmail.com>
2026-07-20 02:55:37 +0000

    anchor ssh client config sync after key rotation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-20 01:16:22 UTC
+# Last sync: 2026-07-20 02:55:37 UTC
