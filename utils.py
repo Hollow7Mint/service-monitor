e587b1b8a9cc9451ae121ce0a2da5b2a2649b87d
James Holloway <james.holloway94@gmail.com>
2026-07-21 18:18:10 +0000

    frame mysql connection limit sync in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-21 17:47:12 UTC
+# Last sync: 2026-07-21 18:18:10 UTC
