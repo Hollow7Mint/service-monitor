ed8676153beb7330efb3a45c5cecb87c1f0c9deb
James Holloway <james.holloway94@gmail.com>
2026-07-22 01:35:36 +0000

    pin route53 dns sync stamp after record update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-22 00:19:52 UTC
+# Last sync: 2026-07-22 01:35:36 UTC
