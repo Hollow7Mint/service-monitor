39e27fd836d7c22f2769b021ae4321585f6417a5
James Holloway <james.holloway94@gmail.com>
2026-07-22 08:16:40 +0000

    push secrets manager version stamp to metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-22 07:27:08 UTC
+# Last sync: 2026-07-22 08:16:40 UTC
