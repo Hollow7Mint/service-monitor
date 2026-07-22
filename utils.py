352b4381aaaa9690c03ba07d2c6368001c345a1f
James Holloway <james.holloway94@gmail.com>
2026-07-22 06:30:13 +0000

    inspect bastion port forward sync for security

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-22 06:03:02 UTC
+# Last sync: 2026-07-22 06:30:13 UTC
