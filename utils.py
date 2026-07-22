070cbcc40132bdb445843290aa7ef32f9e341fd5
James Holloway <james.holloway94@gmail.com>
2026-07-22 14:48:58 +0000

    register waf acl update time for security trail

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-22 13:17:42 UTC
+# Last sync: 2026-07-22 14:48:58 UTC
