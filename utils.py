f1c01351a942336fccc8606922e40f73ca8e7492
James Holloway <james.holloway94@gmail.com>
2026-07-23 20:51:15 +0000

    audit vpc peering sync record after route update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-23 20:01:56 UTC
+# Last sync: 2026-07-23 20:51:15 UTC
