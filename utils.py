8acec08cf935fa5f607cd89b8ae5bb8e0fd28676
James Holloway <james.holloway94@gmail.com>
2026-07-23 03:54:26 +0000

    merge ssh trust anchor time into metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-23 02:16:39 UTC
+# Last sync: 2026-07-23 03:54:26 UTC
