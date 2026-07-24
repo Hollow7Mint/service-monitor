2a0819bf71e47cc9f218e33fdb259a962e94c192
James Holloway <james.holloway94@gmail.com>
2026-07-24 01:13:54 +0000

    pin mongodb shard balancing time in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-24 00:11:27 UTC
+# Last sync: 2026-07-24 01:13:54 UTC
