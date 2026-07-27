aea5745e9a5a0dbd0806833de133ad48c1ac0aee
James Holloway <james.holloway94@gmail.com>
2026-07-27 10:59:27 +0000

    finalize vpc security group sync after rule push

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-27 09:50:39 UTC
+# Last sync: 2026-07-27 10:59:27 UTC
