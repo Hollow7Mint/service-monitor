74f84d14c6166222af3ef815d667c02dc5a1bc59
James Holloway <james.holloway94@gmail.com>
2026-07-27 07:44:55 +0000

    write bastion access log time into metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-27 07:00:30 UTC
+# Last sync: 2026-07-27 07:44:55 UTC
