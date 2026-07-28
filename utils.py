50d7230779787a3e69d7d3f5b99b50d4256f3b35
James Holloway <james.holloway94@gmail.com>
2026-07-28 18:47:48 +0000

    tag known_hosts sync checkpoint for key trail

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-28 17:35:27 UTC
+# Last sync: 2026-07-28 18:47:47 UTC
