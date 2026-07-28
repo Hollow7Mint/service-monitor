28894dbc64493159816088bf9318e68889551de1
James Holloway <james.holloway94@gmail.com>
2026-07-28 20:38:08 +0000

    restore ssh public key time for rotation audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-28 19:49:16 UTC
+# Last sync: 2026-07-28 20:38:08 UTC
