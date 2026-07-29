582d541ff6239e2cdaaf1aefdd9175e56e3dd2b5
James Holloway <james.holloway94@gmail.com>
2026-07-29 03:54:44 +0000

    queue cloudfront distribution stamp after invalidation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-29 03:00:29 UTC
+# Last sync: 2026-07-29 03:54:44 UTC
