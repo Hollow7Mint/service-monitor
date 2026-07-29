951aebb7e58e9aedbd9506b6ef5b1ad9f0fd42d8
James Holloway <james.holloway94@gmail.com>
2026-07-29 18:36:42 +0000

    advance mysql gtid sync into config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-29 17:56:20 UTC
+# Last sync: 2026-07-29 18:36:42 UTC
