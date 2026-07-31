cb834e3040fed92052a5f2dd71b7d87bb2550572
James Holloway <james.holloway94@gmail.com>
2026-07-31 20:12:00 +0000

    ship db schema sync record after nightly apply

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-07-31 19:42:37 UTC
+# Last sync: 2026-07-31 20:12:00 UTC
