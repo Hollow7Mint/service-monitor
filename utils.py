cf52b8162ca380571200815ba79781b2b8268604
James Holloway <james.holloway94@gmail.com>
2026-08-01 10:10:15 +0000

    annotate mysql slow log sync time for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-01 10:03:37 UTC
+# Last sync: 2026-08-01 10:10:15 UTC
