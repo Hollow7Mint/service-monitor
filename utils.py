e85c2b87d5ea0011457ebccc7f4fc5dc55cb7ebc
James Holloway <james.holloway94@gmail.com>
2026-08-01 04:55:25 +0000

    harvest ssh fingerprint sync for nightly audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-01 04:02:13 UTC
+# Last sync: 2026-08-01 04:55:25 UTC
