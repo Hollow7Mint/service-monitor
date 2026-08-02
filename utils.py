f48d4cc282bc7f996bb3dc16f720734ef0eef322
James Holloway <james.holloway94@gmail.com>
2026-08-02 15:04:14 +0000

    store cloudfront ttl sync stamp for cache audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-02 13:30:17 UTC
+# Last sync: 2026-08-02 15:04:14 UTC
