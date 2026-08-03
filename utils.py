5cd22c8227341d15281686bc5062f17c5e3f4700
James Holloway <james.holloway94@gmail.com>
2026-08-03 08:53:20 +0000

    queue ssh agent forwarding sync after config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-03 07:49:11 UTC
+# Last sync: 2026-08-03 08:53:20 UTC
