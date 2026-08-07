a446bc8017a39c607dee2ff928ee9ffaca3c0207
James Holloway <james.holloway94@gmail.com>
2026-08-07 19:44:25 +0000

    run guardduty detector heartbeat for scheduled audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-07 18:15:57 UTC
+# Last sync: 2026-08-07 19:44:24 UTC
