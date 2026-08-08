7b1bba87168044ee86f8893b410e40480a0f9247
James Holloway <james.holloway94@gmail.com>
2026-08-08 03:02:08 +0000

    advance ssh key trust sync into config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-08 01:51:54 UTC
+# Last sync: 2026-08-08 03:02:08 UTC
