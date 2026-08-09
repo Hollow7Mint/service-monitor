07b2f5cd4a702fbc4bb7565819cd46b6ed29b593
James Holloway <james.holloway94@gmail.com>
2026-08-09 09:44:29 +0000

    update iam policy checkpoint for scheduled audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-09 08:53:09 UTC
+# Last sync: 2026-08-09 09:44:29 UTC
