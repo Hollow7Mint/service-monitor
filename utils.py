656ad07848e4f2825e352a963e139f3b0a3cb0d6
James Holloway <james.holloway94@gmail.com>
2026-08-09 04:49:46 +0000

    thread cloudtrail digest sync through config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-09 03:30:21 UTC
+# Last sync: 2026-08-09 04:49:46 UTC
