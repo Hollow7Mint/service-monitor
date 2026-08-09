0021e089fe4537cdb15d6724b23e01ea7e201078
James Holloway <james.holloway94@gmail.com>
2026-08-09 20:32:05 +0000

    anchor mysql parameter sync after maintenance window

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-09 19:15:48 UTC
+# Last sync: 2026-08-09 20:32:05 UTC
