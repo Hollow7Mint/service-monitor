b9eaa2b30a9e955c8d8ba0dd87646044e93e592b
James Holloway <james.holloway94@gmail.com>
2026-08-10 03:23:08 +0000

    write mongodb index rebuild time into metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-10 02:18:35 UTC
+# Last sync: 2026-08-10 03:23:08 UTC
