645294cb17f09bb7785a06972752538bdd1c1bd4
James Holloway <james.holloway94@gmail.com>
2026-08-10 13:36:16 +0000

    relay db connection string time through config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-10 12:39:42 UTC
+# Last sync: 2026-08-10 13:36:16 UTC
