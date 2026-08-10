2f04020900fea54a03e88e597ac775fbaf18bbe0
James Holloway <james.holloway94@gmail.com>
2026-08-10 16:18:55 +0000

    push ssh config sync marker after host refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-10 16:06:10 UTC
+# Last sync: 2026-08-10 16:18:55 UTC
