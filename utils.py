366fce6fcb1b63352ed6f70adfe057399a0349c2
James Holloway <james.holloway94@gmail.com>
2026-08-11 16:50:10 +0000

    preserve ssh certificate revocation sync in metadata

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-11 15:47:46 UTC
+# Last sync: 2026-08-11 16:50:10 UTC
