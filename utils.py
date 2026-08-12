663717af2aec3f8ec472b5d91be5e2c1dc07171e
James Holloway <james.holloway94@gmail.com>
2026-08-12 22:00:30 +0000

    retain redis aof sync stamp for persistence audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-12 20:21:40 UTC
+# Last sync: 2026-08-12 22:00:30 UTC
