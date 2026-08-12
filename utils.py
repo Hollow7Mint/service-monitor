ceba809fe21984bac0ae81d807e3d930d45f6060
James Holloway <james.holloway94@gmail.com>
2026-08-12 18:57:19 +0000

    emit ssh certificate renewal heartbeat for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-12 17:46:35 UTC
+# Last sync: 2026-08-12 18:57:18 UTC
