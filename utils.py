f1b8e513c0cb839ebfec0f0935b7e92cc70a6a67
James Holloway <james.holloway94@gmail.com>
2026-08-12 10:32:36 +0000

    frame ssh agent socket sync in config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-12 09:24:25 UTC
+# Last sync: 2026-08-12 10:32:36 UTC
