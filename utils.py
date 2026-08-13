f77402ab9dd63f713c826103f4f92eb36e17f6cd
James Holloway <james.holloway94@gmail.com>
2026-08-13 20:17:30 +0000

    flush bastion authorized_keys time for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-13 19:41:03 UTC
+# Last sync: 2026-08-13 20:17:30 UTC
