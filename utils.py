7fc4405fc1db9a2fa1c75b9c280433a1314d88dc
James Holloway <james.holloway94@gmail.com>
2026-08-14 02:20:59 +0000

    catalog rds parameter group sync after maintenance

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-14 01:18:08 UTC
+# Last sync: 2026-08-14 02:20:59 UTC
