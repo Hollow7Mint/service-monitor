a85975adfff020be5563d0daa8fe9edbf3b14a62
James Holloway <james.holloway94@gmail.com>
2026-08-15 00:26:45 +0000

    carry mongodb oplog sync stamp to config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-14 23:52:03 UTC
+# Last sync: 2026-08-15 00:26:45 UTC
