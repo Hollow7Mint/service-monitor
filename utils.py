41b90311e04b01757986c7fa6cc6738e1056744d
James Holloway <james.holloway94@gmail.com>
2026-08-15 23:12:16 +0000

    reload postgres autovacuum sync marker for audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-15 22:29:57 UTC
+# Last sync: 2026-08-15 23:12:16 UTC
