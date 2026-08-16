c9d76cacaf8a4e9c88a58910847a67f055510a1d
James Holloway <james.holloway94@gmail.com>
2026-08-16 01:49:12 +0000

    save bastion policy sync marker after review

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-16 00:24:21 UTC
+# Last sync: 2026-08-16 01:49:12 UTC
