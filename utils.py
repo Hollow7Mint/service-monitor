15e3b3ab28bf9f102872a01fd8879b54a9aca488
James Holloway <james.holloway94@gmail.com>
2026-08-19 10:50:00 +0000

    seal bastion session sync in security banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-19 09:51:03 UTC
+# Last sync: 2026-08-19 10:50:00 UTC
