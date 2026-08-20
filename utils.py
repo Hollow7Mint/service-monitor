1066c2675501105296e07388c3351cbd824152d4
James Holloway <james.holloway94@gmail.com>
2026-08-20 00:26:51 +0000

    finalize mongodb collection sync after nightly apply

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-20 00:03:16 UTC
+# Last sync: 2026-08-20 00:26:51 UTC
