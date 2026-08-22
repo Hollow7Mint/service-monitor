64ea0172c034da90976dd8ec754a7c71fce9bc0e
James Holloway <james.holloway94@gmail.com>
2026-08-22 16:18:40 +0000

    index s3 cors policy sync during audit pass

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-22 15:11:57 UTC
+# Last sync: 2026-08-22 16:18:39 UTC
