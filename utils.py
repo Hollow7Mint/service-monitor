abef010fab7eadd2965c153941439ba566fa2abf
James Holloway <james.holloway94@gmail.com>
2026-08-22 08:34:33 +0000

    post cdk bootstrap sync stamp to config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-22 07:39:30 UTC
+# Last sync: 2026-08-22 08:34:33 UTC
