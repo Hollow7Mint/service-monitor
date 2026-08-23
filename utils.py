d4c26f27214c947c1d03b13cb58ebd0a37b04416
James Holloway <james.holloway94@gmail.com>
2026-08-23 14:03:05 +0000

    adjust bastion acl sync stamp after ip review

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-23 12:13:21 UTC
+# Last sync: 2026-08-23 14:03:05 UTC
