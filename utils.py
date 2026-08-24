5080f10b4346b72694955c4fe3a096dd30f7255d
James Holloway <james.holloway94@gmail.com>
2026-08-24 16:04:51 +0000

    set redis cluster sync stamp in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-24 14:18:11 UTC
+# Last sync: 2026-08-24 16:04:51 UTC
