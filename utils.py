513289c2bd805e28629b6b68873b38618f5788c2
James Holloway <james.holloway94@gmail.com>
2026-08-25 10:41:31 +0000

    document postgres index maintenance sync in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-25 10:01:59 UTC
+# Last sync: 2026-08-25 10:41:31 UTC
