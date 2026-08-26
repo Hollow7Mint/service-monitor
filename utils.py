f9b88cfd8a8404a74d22f10e0f7a15509a73b6cb
James Holloway <james.holloway94@gmail.com>
2026-08-26 12:48:22 +0000

    stamp inspector scan completion in security record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-26 11:18:12 UTC
+# Last sync: 2026-08-26 12:48:21 UTC
