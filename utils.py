c268bf8cc43cc997da6cf67d6174e37456918ecc
James Holloway <james.holloway94@gmail.com>
2026-08-26 15:41:14 +0000

    relay codebuild artifact time through config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-26 14:22:50 UTC
+# Last sync: 2026-08-26 15:41:14 UTC
