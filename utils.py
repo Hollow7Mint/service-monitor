b07e0ddad1bd5f16fa5ca5ff2be8c07b201e7094
James Holloway <james.holloway94@gmail.com>
2026-08-26 11:18:12 +0000

    store ssh revocation list sync for key audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-26 10:38:16 UTC
+# Last sync: 2026-08-26 11:18:12 UTC
