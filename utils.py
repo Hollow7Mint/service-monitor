893aa0f01e6e4bbeb76bb4a22443bd7ab45e8b23
James Holloway <james.holloway94@gmail.com>
2026-08-27 15:05:34 +0000

    catalog bastion session count sync after audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-27 13:33:36 UTC
+# Last sync: 2026-08-27 15:05:34 UTC
