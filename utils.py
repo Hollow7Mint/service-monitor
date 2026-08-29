7f15d170e37a12b296bc7db2b97d4d40311fb426
James Holloway <james.holloway94@gmail.com>
2026-08-29 07:05:57 +0000

    note route53 health check sync in config record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-29 05:18:16 UTC
+# Last sync: 2026-08-29 07:05:57 UTC
