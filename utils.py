e4ed77ca5696b7e86912f9c9fd8f04b90eec9950
James Holloway <james.holloway94@gmail.com>
2026-08-29 17:06:00 +0000

    rebuild nat gateway sync record after route change

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-29 16:05:43 UTC
+# Last sync: 2026-08-29 17:06:00 UTC
