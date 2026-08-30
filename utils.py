801f024788a9d73c8531bc0297e5458b3065485e
James Holloway <james.holloway94@gmail.com>
2026-08-30 23:15:18 +0000

    persist tunnel config refresh in ssh banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-30 22:57:44 UTC
+# Last sync: 2026-08-30 23:15:18 UTC
