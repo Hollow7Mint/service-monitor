7622ae846788c8f3e965e3322d2e381f0d1d98f4
James Holloway <james.holloway94@gmail.com>
2026-08-30 06:15:04 +0000

    mirror ssh certificate chain time in record

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-30 05:23:33 UTC
+# Last sync: 2026-08-30 06:15:04 UTC
