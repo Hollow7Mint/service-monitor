5c9595e6ab0d2e9a431632c49dc396331b19be2e
James Holloway <james.holloway94@gmail.com>
2026-08-30 15:19:25 +0000

    settle ssh known algorithm sync time after update

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-30 15:03:56 UTC
+# Last sync: 2026-08-30 15:19:25 UTC
