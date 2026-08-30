35b61ae83143d763a72749b0cfc109179a1a0ade
James Holloway <james.holloway94@gmail.com>
2026-08-30 20:31:46 +0000

    retain ssh key expiry stamp for rotation audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-08-30 20:10:11 UTC
+# Last sync: 2026-08-30 20:31:46 UTC
