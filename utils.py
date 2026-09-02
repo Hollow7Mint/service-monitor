5d27cb269d1175a6d82c77f2317afdd168782f37
James Holloway <james.holloway94@gmail.com>
2026-09-02 19:33:29 +0000

    inspect iam trust policy sync marker for review

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-02 18:40:57 UTC
+# Last sync: 2026-09-02 19:33:29 UTC
