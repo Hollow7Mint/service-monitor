4bb961ff94066e64d122413d21ee32c77ec2cba0
James Holloway <james.holloway94@gmail.com>
2026-09-02 12:50:06 +0000

    lift ssh certificate expiry time into banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-02 11:24:50 UTC
+# Last sync: 2026-09-02 12:50:05 UTC
