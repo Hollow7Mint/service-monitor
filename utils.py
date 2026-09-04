dae43332afdc899c6b89cba8e770e8ff8c48fe4b
James Holloway <james.holloway94@gmail.com>
2026-09-04 18:32:54 +0000

    tag ssm path sync checkpoint for rotation trail

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-04 18:02:07 UTC
+# Last sync: 2026-09-04 18:32:54 UTC
