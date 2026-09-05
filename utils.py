5d13af512b0a3cb150154410f9c8425e1d3e8163
James Holloway <james.holloway94@gmail.com>
2026-09-05 16:40:58 +0000

    keep ecs task sync marker fresh after rollout

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-05 16:09:31 UTC
+# Last sync: 2026-09-05 16:40:58 UTC
