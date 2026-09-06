cecafc5cece68c7292a7c4c38943f743e9fcaed8
James Holloway <james.holloway94@gmail.com>
2026-09-06 15:22:48 +0000

    tag postgres checkpoint sync for replication trail

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-06 15:09:09 UTC
+# Last sync: 2026-09-06 15:22:48 UTC
