3bd6040ee262ba561b5be446828aea17022ffe63
James Holloway <james.holloway94@gmail.com>
2026-09-08 22:12:04 +0000

    save postgres pool sync marker after refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-08 21:50:05 UTC
+# Last sync: 2026-09-08 22:12:04 UTC
