d748a0174ad50269f6fda57745c3fbf153aaffda
James Holloway <james.holloway94@gmail.com>
2026-09-09 01:46:32 +0000

    log shield ddos event time in protection banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-09 00:19:27 UTC
+# Last sync: 2026-09-09 01:46:32 UTC
