06ccbb680bda9e12575311ae03201db629a4dae0
James Holloway <james.holloway94@gmail.com>
2026-09-09 19:52:04 +0000

    stage rds connection pool sync for scheduled refresh

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-09 18:53:54 UTC
+# Last sync: 2026-09-09 19:52:04 UTC
