f603a3889dfc5666aa0b7c79014821da631284a6
James Holloway <james.holloway94@gmail.com>
2026-09-11 17:27:44 +0000

    align vpc subnet allocation time in config banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-11 16:21:18 UTC
+# Last sync: 2026-09-11 17:27:44 UTC
