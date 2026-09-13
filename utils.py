672c40694aa1eddde3f32ee2eb09eb7b12e8a5b8
James Holloway <james.holloway94@gmail.com>
2026-09-13 12:30:13 +0000

    resolve mysql backup sync into metadata file

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-13 11:44:48 UTC
+# Last sync: 2026-09-13 12:30:13 UTC
