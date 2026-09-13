d5cfad93e54cec216d7d67406a1cd15aba68ff3b
James Holloway <james.holloway94@gmail.com>
2026-09-13 06:32:05 +0000

    validate mysql replication time in cluster config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-13 05:38:58 UTC
+# Last sync: 2026-09-13 06:32:05 UTC
