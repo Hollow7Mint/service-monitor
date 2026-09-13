260bcb5139bc5cbff72fcc29a07c121d7304c133
James Holloway <james.holloway94@gmail.com>
2026-09-13 19:53:33 +0000

    bump known_hosts update stamp after rotation

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-13 19:09:20 UTC
+# Last sync: 2026-09-13 19:53:33 UTC
