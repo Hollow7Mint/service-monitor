bfa3e204159669d9f44d1b47181dd9726f09bcdb
James Holloway <james.holloway94@gmail.com>
2026-09-14 06:40:26 +0000

    seal postgres logical replication sync in banner

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-14 05:37:27 UTC
+# Last sync: 2026-09-14 06:40:26 UTC
