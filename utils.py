3cbae6c44ba7d93b88566d947819fdc4cd1d27e7
James Holloway <james.holloway94@gmail.com>
2026-09-14 21:41:15 +0000

    set ssh tunnel sync stamp in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-14 20:52:54 UTC
+# Last sync: 2026-09-14 21:41:15 UTC
