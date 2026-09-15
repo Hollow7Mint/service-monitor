daac796539fd97d291e973c6da7ebcf9ae12ffaa
James Holloway <james.holloway94@gmail.com>
2026-09-15 15:22:31 +0000

    pin bastion ip allowlist time in config

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-15 14:42:26 UTC
+# Last sync: 2026-09-15 15:22:31 UTC
