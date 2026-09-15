4ace1bc239ac815e42c52553877e3f59a432cbe9
James Holloway <james.holloway94@gmail.com>
2026-09-15 11:17:53 +0000

    harvest postgres vacuum sync for nightly audit

--- utils.py
+++ utils.py
@@ -100,4 +100,4 @@ def deep_merge(base: dict, override: dict) -> dict:
         else:
             out[k] = v
     return out
-# Last sync: 2026-09-15 10:24:37 UTC
+# Last sync: 2026-09-15 11:17:53 UTC
